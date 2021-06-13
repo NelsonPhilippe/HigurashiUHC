7481d4f95954f8e95baa4baae7da406b9454a1ec
xilitra <nenelphinel59@gmail.com>
2021-06-13 16:29:51 +0200

    initial commit

---
+++ src/main/java/fr/xilitra/higurashiuhc/HigurashiUHC.java
@@ -0,0 +1,17 @@
+package fr.xilitra.higurashiuhc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HigurashiUHC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}

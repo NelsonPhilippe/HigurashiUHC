a936cb9429886dfca7fd94296eb7a1bf54b447de
RedLine <ninogmz33@gmail.com>
2022-01-25 20:14:59 +0100

    Fix

--- src/main/java/fr/xilitra/higurashiuhc/roles/NullAction.java
+++ src/main/java/fr/xilitra/higurashiuhc/roles/NullAction.java
@@ -1,9 +1,5 @@
 package fr.xilitra.higurashiuhc.roles;
 
-import fr.xilitra.higurashiuhc.player.HPlayer;
-import fr.xilitra.higurashiuhc.utils.DeathReason;
-import org.bukkit.entity.Player;
-
 public class NullAction implements RoleAction{
     @Override
     public String getDescription() {
